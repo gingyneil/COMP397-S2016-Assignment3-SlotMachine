a95328cfe21d0fb2fc0f48f2130a31b950293669
gingyneil <neil.reading.nr@gmail.com>
2016-07-10 19:51:36 -0400

    Added background image and default slot images

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,27 +1,59 @@
 ///<reference path="reference.ts"/>
+//VARIABLES*********************************
+var slotItem = [];
+//GAME OBJECTS
+var background;
+var game;
 // make a reference to the canvas element
 var canvas = document.getElementById("canvas");
 // create a ference to a stage container
 var stage;
-var helloLabel;
+//let helloLabel:createjs.Text;
 function init() {
     stage = new createjs.Stage(canvas); // instantiates the stage container
     createjs.Ticker.framerate = 60; // frame rate
     createjs.Ticker.on("tick", gameLoop); // create an event listener for the tick event
-    main(); // call the main game function 
+    // call the main game function 
+    main();
 }
+//GAME LOOP
 function gameLoop() {
-    helloLabel.rotation += 5;
+    /* helloLabel.rotation += 5;*/
     stage.update(); // refreshes the page
 }
 function main() {
-    helloLabel = new createjs.Text("Hello world!", "40px Arial", "#00000");
+    game = new createjs.Container();
+    createUI();
+    /*helloLabel = new createjs.Text("Hello world!", "40px Arial", "#00000");
     helloLabel.regX = helloLabel.getMeasuredWidth() * .5;
     helloLabel.regY = helloLabel.getMeasuredHeight() * .5;
     helloLabel.x = 320;
     helloLabel.y = 240;
     stage.addChild(helloLabel);
-    helloLabel.text;
+
+    helloLabel.text
+    */
+    stage.addChild(game);
 }
 window.addEventListener("load", init);
+function createUI() {
+    //Add the slot machine image to the background of the game screen
+    background = new createjs.Bitmap("assets/images/slotmachine.png");
+    background.x = (640 - background.image.width) / 2;
+    background.y = (640 - background.image.height) / 2;
+    game.addChild(background);
+    //Add 3 slot machine items for the game and set positions
+    slotItem[0] = new createjs.Bitmap("assets/images/hulk.jpg");
+    slotItem[1] = new createjs.Bitmap("assets/images/ironman.jpg");
+    slotItem[2] = new createjs.Bitmap("assets/images/Thor.jpg");
+    game.addChild(slotItem[0]);
+    game.addChild(slotItem[1]);
+    game.addChild(slotItem[2]);
+    slotItem[0].x = 90;
+    slotItem[0].y = 200;
+    slotItem[1].x = 250;
+    slotItem[1].y = 200;
+    slotItem[2].x = 415;
+    slotItem[2].y = 200;
+}
 //# sourceMappingURL=game.js.map
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,21 +1,32 @@
 ///<reference path="reference.ts"/>
 
+
+//VARIABLES*********************************
+let slotItem: createjs.Bitmap[] = [];
+
+//GAME OBJECTS
+let background: createjs.Bitmap;
+let game: createjs.Container;
+
 // make a reference to the canvas element
 let canvas:HTMLElement = document.getElementById("canvas");	
 // create a ference to a stage container
 let stage:createjs.Stage;
 
-let helloLabel:createjs.Text;
+//let helloLabel:createjs.Text;
 
 function init():void {
     stage = new createjs.Stage(canvas);// instantiates the stage container
     createjs.Ticker.framerate = 60; // frame rate
     createjs.Ticker.on("tick", gameLoop);    // create an event listener for the tick event
-    main(); // call the main game function 
+   
+   // call the main game function 
+    main(); 
 }
 
+//GAME LOOP
 function gameLoop():void {
-    helloLabel.rotation += 5;
+   /* helloLabel.rotation += 5;*/
     
     
     stage.update(); // refreshes the page
@@ -24,7 +35,11 @@ function gameLoop():void {
 
 
 function main():void {
-    helloLabel = new createjs.Text("Hello world!", "40px Arial", "#00000");
+   game = new createjs.Container();
+   
+   createUI();
+
+    /*helloLabel = new createjs.Text("Hello world!", "40px Arial", "#00000");
     helloLabel.regX = helloLabel.getMeasuredWidth() * .5;
     helloLabel.regY = helloLabel.getMeasuredHeight() * .5;
     helloLabel.x = 320;
@@ -32,5 +47,32 @@ function main():void {
     stage.addChild(helloLabel);
 
     helloLabel.text
+    */
+
+    stage.addChild(game);
 }
 window.addEventListener("load", init);
+
+function createUI() {
+    
+    //Add the slot machine image to the background of the game screen
+    background = new createjs.Bitmap("assets/images/slotmachine.png");
+    background.x = (640 - background.image.width) / 2;
+    background.y = (640 - background.image.height) /2;
+    game.addChild(background);
+
+    //Add 3 slot machine items for the game and set positions
+    slotItem[0] = new createjs.Bitmap("assets/images/hulk.jpg");
+    slotItem[1] = new createjs.Bitmap("assets/images/ironman.jpg");
+    slotItem[2] = new createjs.Bitmap("assets/images/Thor.jpg");
+    game.addChild(slotItem[0]);
+    game.addChild(slotItem[1]);
+    game.addChild(slotItem[2]);
+    slotItem[0].x = 90;
+    slotItem[0].y = 200;
+    slotItem[1].x = 250;
+    slotItem[1].y = 200;
+    slotItem[2].x = 415;
+    slotItem[2].y = 200;
+
+}
